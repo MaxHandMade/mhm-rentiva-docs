92d97ef3946d2ffde36cb2f8ab2d96b1a414c782
MaxHandMade <MaxHandMade@users.noreply.github.com>
2026-04-24 05:34:56 +0000

    deploy: 76151a93260b0c782d26e845322255b6cdfe94e9

---
+++ tr/assets/js/acecf23e.8843a6b2.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/mhm-rentiva-docs/tr/blog","blogTitle":"Blog","authorsListPath":"/mhm-rentiva-docs/tr/blog/authors"}')}}]);
